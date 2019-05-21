922bdb53eacd044171da3d8ab4be5b7ba466f062
KellySchumann <kellyschumann@StrongroomSolutions.com>
2019-05-21 08:52:20 -0500

    Added some Debugging statements to verify amount completed.

--- run_analysis.R
+++ run_analysis.R
@@ -4,7 +4,7 @@ library(dplyr)
 ## This is the actual assignment
 ##
 run_analysis <- function(myn = -100){
-  #ptm <- proc.time()
+  ptm <- proc.time()
   setwd("C:\\Kelly\\Coursera\\Course 3 - Getting and Cleaning Data\\Assignment\\GetCleanData\\data")
   dir <- getwd()
   
@@ -57,6 +57,8 @@ run_analysis <- function(myn = -100){
   #print (paste("trainAct: (",ncol(trainAct),",",nrow(trainAct),")"))
   ## Add the Subject to the DataSet
   trainSub <- read.csv2(paste(dir,"train","subject_train.txt", sep="\\"), col.names = c("Subject"), nrows = myn, header=FALSE, blank.lines.skip=TRUE)
+  print (paste("training set: (",ncol(trainX), ",", nrow(trainX),",",nrow(trainAct), ",", nrow(trainSub),")"))
+  
   
   ## Read the Test Dataset
   testX <- read.fwf(paste(dir,"test","X_test.txt", sep="\\"), widths = rep(16,561), n = myn, col.names = valid_names, blank.lines.skip=TRUE)
@@ -68,6 +70,7 @@ run_analysis <- function(myn = -100){
   #print (paste("testAct: (",ncol(testAct),",",nrow(testAct),")"))
   ## Add the Subject to the DataSet
   testSub <- read.csv2(paste(dir,"test","subject_test.txt", sep="\\"), col.names = c("Subject"), nrows = myn, header=FALSE, blank.lines.skip=TRUE)
+  print (paste("test set: (",ncol(testX), ",", nrow(testX),",",nrow(testAct), ",", nrow(testSub),")"))
   
   
   #Merge the Test and Train DataSets together
@@ -76,12 +79,14 @@ run_analysis <- function(myn = -100){
   completeAct <- rbind(testAct, trainAct)
   #print (paste("completeAct: (",ncol(completeAct),",",nrow(completeAct),")"))
   completeSub <- rbind(testSub, trainSub)
-
+  print (paste("complete set: (",ncol(completeDS), ",", nrow(completeDS),",",nrow(completeAct), ",", nrow(completeSub),")"))
+  
   ##
   ##                                   Question 1 - One Data set
   ##
   completeDS <- cbind(completeDS, completeAct, completeSub, deparse.level = 1)   #Question 4 dataset with descriptive name
-
+  print("Done Combining into 1 Data Set")
+  
   ## Get the indices (column) of the dataset containing the mean or Standard Deviation
   indx <- grep("mean|std", labels[,2])
   indx[length(indx)+1] <- length(labels$V2)+1 ## Add the Activity Column
@@ -93,7 +98,7 @@ run_analysis <- function(myn = -100){
   ##
   ##  added activity to understand the activity involved, and subject
   means.std <- completeDS[,indx]
-
+  
   ## Add the Activity to the means.std DataSet
   ActivityName <- read.csv(paste(dir,"activity_labels.txt", sep="\\"), header=FALSE, sep=" ", col.names = c("Activity","ActivityName"))
   
@@ -103,6 +108,7 @@ run_analysis <- function(myn = -100){
   ##
   ## for readability to means.std
   means.std <- inner_join(means.std, ActivityName, by= c("Activity","Activity"))
+  print("Done creating means.std Data Set")
   
 
   ##  
@@ -116,7 +122,9 @@ run_analysis <- function(myn = -100){
   ave.activity <- aggregate(means.std[,1:(length(means.std)-2)], list(means.std$Subject, means.std$ActivityName), mean)
   names(ave.activity)[1] <- "Subject"
   names(ave.activity)[2] <- "Activity"
-
-    write.table(ave.activity, "tidyset2.txt", row.names = FALSE)
+  print("Done creating grouped averages Data Set")
+  
+  write.table(ave.activity, "tidyset2.txt", row.names = FALSE)
+  print(proc.time() - ptm)
 }
 
